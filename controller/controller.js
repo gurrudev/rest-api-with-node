abcde11b59cb508bc1d9ba4eb445a2f2dc6c0811
ASHUTOSH PAWAR <80522156+gurrudev@users.noreply.github.com>
2023-04-12 18:27:37 +0530

    updated

---
+++ controller/controller.js
@@ -0,0 +1,46 @@
+import Data from "../models/dataSchema";
+
+class CrusOperation {
+
+    static getApiData = async (req, res) =>{
+        try {
+            const data = await Data.find();
+            console.log(data);
+            res.json(data);
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    static postApiData = async (req, res) =>{
+        try {
+            const data = await Data.create(req.body);
+            console.log(data);
+            res.json(data);
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    static putApiData = async (req, res) =>{
+        try {
+            const data = await Data.findByIdAndUpdate(req.params.id, req.body);
+            console.log(data);
+            res.json(data);
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    static deleteApiData = async (req, res) =>{
+        try {
+            const data = await Data.findByIdAndDelete(req.params.id);
+            console.log(data);
+            res.json(data);
+        } catch (err) {
+            console.log(err)
+        }
+    }
+}
+
+export default CrusOperation;
