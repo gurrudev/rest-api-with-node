48d02f4760ec0d21841f20d58ac87be54dd8a75f
ASHUTOSH PAWAR <80522156+gurrudev@users.noreply.github.com>
2023-04-12 17:38:44 +0530

    a

--- app.js
+++ app.js
@@ -1,19 +1,15 @@
-
-import  express  from "express";
-import  bodyParser  from "body-parser";
+import express from "express";
+import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import Data from "./models/dataSchema.js";
-import dotenv from "dotenv"
-
+import dotenv from "dotenv";
 import Dbconnect from "./config/dbConnect.js";
 
-
-  Dbconnect.then(() => {
+Dbconnect.then(() => {
     console.log("monogDB connected successfully");
-  })
-  .catch((error) => {
+}).catch((error) => {
     console.log(error);
-  });
+});
 
 const app = express();
 
@@ -23,35 +19,34 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get("/data", async (req, res) => {
-  const data = await Data.find();
-  console.log(data);
-  res.json(data);
+    const data = await Data.find();
+    console.log(data);
+    res.json(data);
 });
 
 app.post("/user", async (req, res) => {
     try {
-      let user = new Data(req.body)
-      await user.save()
-      res.send(user)
-        console.log(user)
+        let user = new Data(req.body);
+        await user.save();
+        res.send(user);
+        console.log(user);
     } catch (error) {
-        res.send(error)
+        res.send(error);
     }
 });
 
 app.put("/data/:id", (req, res) => {
-  Data.findByIdAndUpdate(req.params.id, req.body, (err) => {
-    res.json({ message: `updated data ${req.params.id}` });
-  });
+    Data.findByIdAndUpdate(req.params.id, req.body, (err) => {
+        res.json({ message: `updated data ${req.params.id}` });
+    });
 });
 
-
-app.delete('/data/:id', (req, res) => {
-    Data.findByIdAndDelete(req.params.id ,req.body, (err) =>{
+app.delete("/data/:id", (req, res) => {
+    Data.findByIdAndDelete(req.params.id, req.body, (err) => {
         res.json({ message: `deleted data ${req.params.id}` });
-    })
+    });
 });
 
 app.listen(port, () => {
-  console.log(`Server started on: ${port}`);
+    console.log(`Server started on: ${port}`);
 });
